936007b5111443ae40c0fdeed31e60730e454aab
foxmon <jjockrod@naver.com>
2023-01-30 22:25:45 +0900

    is

---
+++ src/utils/is.ts
@@ -0,0 +1,13 @@
+export namespace Is {
+  export function isArray(value: unknown): boolean {
+    return Array.isArray(value);
+  }
+
+  export function isString(value: unknown): boolean {
+    return typeof value === "string";
+  }
+
+  export function isNull(value: unknown): boolean {
+    return value === value;
+  }
+}
